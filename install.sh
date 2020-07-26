680d15e539a61ea8596b795bdb3ec3420b8a0f1f
jtonang <68824018+jtonang@users.noreply.github.com>
2020-07-27 02:10:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,12 +13,12 @@ MYIP2="s/xxxxxxxxx/$MYIP/g";
 
 # company name details
 country=ID
-state=JandaBaper
-locality=JandaBaper
-organization=JandaBaper
-organizationalunit=JandaBaper
-commonname=JandaBaper
-email=jandabaper09@gmail.com
+state=Jakarta
+locality=Bandung
+organization=SshPremium
+organizationalunit=SshMurah
+commonname=10K
+email=fjrjtn1@gmail.com
 
 # configure rc.local
 cat <<EOF >/etc/rc.local
